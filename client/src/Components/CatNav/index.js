da73a5b1670e386f3c52255498137236a82c33a7
Xueyin Bai <xueyin0103@gmail.com>
2024-02-08 16:43:49 -0600

    configure reducers and actions for CatNav

--- client/src/Components/CatNav/index.js
+++ client/src/Components/CatNav/index.js
@@ -1,10 +1,16 @@
-import { useSelector } from 'react-redux';
-import categorySlice from '../../store/slices/categorySlice';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './_cat-nav.scss';
+import { getCategories } from '../../Redux/Category/actions';
 
 
 const CatNav = () => {
-    const categories = useSelector (categorySlice.getInitialState);
+    const categories = useSelector(state=>state.categoryReducer.categories);
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch(getCategories())
+    },[])
 
     return(
         <>
---
+++ client/src/Redux/store/index.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import categorySlice from "../Category/categorySlice";
+
+export const store = configureStore({
+    reducer:{
+        categoryReducer: categorySlice
+  
+    }
+})
