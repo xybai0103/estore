36739821948b1d0fbbbcecdaf2d47825d4c8cc5d
Xueyin Bai <xueyin0103@gmail.com>
2024-02-08 15:35:30 -0600

    configure route

--- server/index.js
+++ server/index.js
@@ -1,28 +1,8 @@
 const express = require ('express');
+const productCategories = require('./Routes/productCategories');
 const app = express();
-const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "Bxy04250201!",
-    database: "estore",
-    port: 3306,
-    multipleStatements: true
-})
-
-app.get("/",(req,res)=>{
-    let categorydata;
-    pool.query("Select * from categories", (error, categories)=>{
-        if(error){
-            categorydata = error;
-            res.status(500).send(categorydata);
-        }else{
-            categorydata = categories;
-            res.status(200).send(categorydata);
-        }
-    })
-})
+app.use("/productCategories",productCategories);
 
 const PORT = 5001;
 
