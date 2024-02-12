4cef696e1164f72ead81f51c658f2fec155ce3a0
Xueyin Bai <xueyin0103@gmail.com>
2024-02-12 14:44:12 -0600

    create cart button

--- client/src/Components/Products/index.js
+++ client/src/Components/Products/index.js
@@ -32,6 +32,16 @@ const Products = () => {
                                     <i className='fa fa-star'/>
                                 </div>
                             </div>
+                            <div className='my-3'>
+                                <div className='cart-button'>
+                                    <div className='cart-icon-container'>
+                                        <i className='fa fa-shopping-cart mx-4'/>
+                                    </div>
+                                    <div className='cart-text-container mx-3'>
+                                        <p>Add to Cart</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     )
                 })
