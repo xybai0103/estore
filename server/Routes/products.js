1550e99ade8accc1912ccacdea985449cb069e7c
Xueyin Bai <xueyin0103@gmail.com>
2024-02-12 14:08:36 -0600

    add track files

---
+++ server/Routes/products.js
@@ -0,0 +1,26 @@
+const express = require ('express');
+const products = express.Router();
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "Bxy04250201!",
+    database: "estore",
+    port: 3306,
+    multipleStatements: true
+})
+
+products.get("/getProducts",(req,res)=>{
+    let productData;
+    pool.query("Select * from products", (err, products)=>{
+        if(err){
+            res.status(500).send(err);
+        }else{
+            productData = products;
+            res.status(200).send(productData);
+        }
+    })
+})
+
+module.exports = products;
