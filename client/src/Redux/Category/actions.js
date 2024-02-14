c5a86fc9ea835eeb2a04a534c82bee75cae427a4
Xueyin Bai <xueyin0103@gmail.com>
2024-02-14 10:18:42 -0600

    change API

--- client/src/Redux/Category/actions.js
+++ client/src/Redux/Category/actions.js
@@ -3,7 +3,8 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 export const getCategories = createAsyncThunk (
     'getCategories',
     ()=>{
-        const categories = fetch("http://localhost:5001/productCategories")
+        // const categories = fetch("http://localhost:5001/productCategories")
+        const categories = fetch("https://estore24-bd1f2ca2cf40.herokuapp.com/productCategories")
         .then((res)=>res.json());
         return categories;
     }
