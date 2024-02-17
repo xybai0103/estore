9549f208a8d5008e9f5c3fd990e6093a10691984
Xueyin Bai <xueyin0103@gmail.com>
2024-02-16 23:21:55 -0600

    configure

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require ('express');
+const path = require('path');
 const productCategories = require('./Routes/productCategories');
 const products = require("./Routes/products");
 const app = express();
@@ -7,12 +8,11 @@ require('dotenv').config();
 
 app.use(cors());
 
-// Serve static files from the React app build directory
-// app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
   
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.use("/productCategories",productCategories);
 app.use("/",products);
