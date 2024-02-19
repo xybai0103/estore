3395117c4f08af6f483b2ea49244924546756091
Xueyin Bai <xueyin0103@gmail.com>
2024-02-19 15:37:56 -0600

    serve static files and catch-all handlers

--- server/index.js
+++ server/index.js
@@ -10,6 +10,14 @@ app.use(cors());
 app.use("/productCategories",productCategories);
 app.use("/getProducts",products);
 
+// Serve static files from the React app build directory
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Catch-all handler for serving the index.html of your React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 const PORT = process.env.PORT || 5001;
 
 const server = app.listen(PORT, ()=>{
